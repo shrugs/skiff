ad21c1b8886bcf6ebea374b66c0ef45ae5eff96f
Matt Condon <MattCMultimedia@gmail.com>
2014-07-03 22:34:49 -0400

    moved DomainRecord to Domain and implemented

--- skiff/Domain.py
+++ skiff/Domain.py
@@ -2,6 +2,20 @@ import requests
 from .utils import DO_BASE_URL, DO_HEADERS, DO_DELETE_HEADERS
 
 
+class SkiffDomainRecord(object):
+    """SkiffDomainRecord"""
+    def __init__(self, domain, options=None, **kwargs):
+        super(SkiffDomainRecord, self).__init__()
+        if not options:
+            options = kwargs
+
+        self.__dict__.update(options)
+        self.domain = domain
+
+    def delete(self):
+        return delete_record(self.id)
+
+
 def delete_domain(did):
     r = requests.delete(DO_BASE_URL + "/domains/" + str(did), headers=DO_DELETE_HEADERS)
     return r.status_code == 204
@@ -15,9 +29,50 @@ class SkiffDomain(object):
             options = kwargs
 
         self.__dict__.update(options)
+        self.record = self.get_record
+        self.create = self.create_record
 
     def delete(self):
         return delete_domain(self.name)
+
+    def records(self):
+        r = requests.get(DO_BASE_URL + '/domains/' + str(self.name) + '/records', headers=DO_HEADERS)
+        r = r.json()
+        return [SkiffDomainRecord(record) for record in r["domain_records"]]
+
+    def create_record(self, options=None, **kwargs):
+        if not options:
+            options = kwargs
+
+        r = requests.post(DO_BASE_URL + '/domains/' + str(self.name) + '/records', data=options, headers=DO_HEADERS)
+        r = r.json()
+        if "message" in r:
+            raise ValueError(r["message"])
+        else:
+            return SkiffDomainRecord(r["domain_record"])
+
+    def delete_record(self, record):
+        # @TODO: if is SkiffRecord, grab that property
+        r = requests.delete(DO_BASE_URL + '/domains/' + str(self.name) + '/records/' + str(record), headers=DO_DELETE_HEADERS)
+        return r.status_code == 204
+
+    def get_record(self, record):
+        # @TODO: if is SkiffRecord, grab that property
+        r = requests.get(DO_BASE_URL + '/domains/' + str(self.name) + '/records' + str(record), headers=DO_HEADERS)
+        r = r.json()
+        return SkiffDomainRecord(r["domain_record"])
+
+    def update_record(self, record, new_name):
+        # @TODO: if is SkiffRecord, grab that property
+        options = {
+            "name": new_name
+        }
+        r = request.put(DO_BASE_URL + '/domains/' + str(self.name) + '/records/' + str(record), data=options, headers=DO_HEADERS)
+        r = r.json()
+        if "message" in r:
+            raise ValueError(r["message"])
+        else:
+            return SkiffDomainRecord(r["domain_record"])
 
 
 def all():
