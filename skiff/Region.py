aeed6c8a389133d7cec57851d72d51eb0f2be3ce
Matt Condon <MattCMultimedia@gmail.com>
2014-07-03 22:48:24 -0400

    implement Region

---
+++ skiff/Region.py
@@ -0,0 +1,22 @@
+import requests
+from .utils import DO_BASE_URL, DO_HEADERS
+
+
+class SkiffRegion(object):
+    """SkiffRegion"""
+    def __init__(self, options=None, **kwargs):
+        super(SkiffRegion, self).__init__()
+        if not options:
+            options = kwargs
+
+        self.__dict__.update(options)
+
+
+def all():
+    r = requests.get(DO_BASE_URL + '/regions', headers=DO_HEADERS)
+    r = r.json()
+    return [SkiffRegion(a) for a in r["regions"]]
+
+
+def get(domain):
+    return all()['domain']
