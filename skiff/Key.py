78fb86e116da30715ec2d9d6b8f08db3cad800da
Matt Condon <MattCMultimedia@gmail.com>
2014-07-05 23:26:36 -0400

    fix Keys problems

--- skiff/Key.py
+++ skiff/Key.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from .utils import DO_BASE_URL, DO_HEADERS, DO_DELETE_HEADERS
 
 
 class SkiffKey(object):
@@ -10,30 +11,38 @@ class SkiffKey(object):
             options = kwargs
 
         self.__dict__.update(options)
+        self.delete = self.destroy
 
     def __repr__(self):
-        return '<' + self.name + ' (#' + self.id + ')>'
+        return '<' + self.name + '(#' + (str(self.id) or '??') + ')>'
 
     def update(self, new_name):
-        r = requests.put(DO_BASE_URL + '/account/keys/' + sr(self.id), data=json.dumps(options), headers=DO_HEADERS)
+        options = {
+            'name': new_name
+        }
+        r = requests.put(DO_BASE_URL + '/account/keys/' + str(self.id), data=json.dumps(options), headers=DO_HEADERS)
         r = r.json()
         return SkiffKey(r["ssh_key"])
 
     def destroy(self):
-        r = requests.delete(do_url + "/account/keys/" + str(self.id), headers=DO_DELETE_HEADERS)
+        r = requests.delete(DO_BASE_URL + "/account/keys/" + str(self.id), headers=DO_DELETE_HEADERS)
         return r.status_code == 204
 
 
 def get(kid):
     r = requests.get(DO_BASE_URL + '/account/keys/' + str(kid), headers=DO_HEADERS)
-    return SkiffKey(r.json())
+    r = r.json()
+    if 'message' in r:
+        raise ValueError(r['message'])
+    else:
+        return SkiffKey(r['ssh_key'])
 
 
 def create(options=None, **kwargs):
     if not options:
         options = kwargs
     r = requests.post(DO_BASE_URL + '/account/keys', data=json.dumps(options), headers=DO_HEADERS)
-    return SkiffKey(r.json()["droplet"])
+    return SkiffKey(r.json()["key"])
 
 
 # alias new to create
