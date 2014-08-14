61ce2e37b49f347961cf21a57e7d544b4d5119fe
Matt Condon <MattCMultimedia@gmail.com>
2014-08-14 13:24:58 -0500

    created SkiffClass + rig constructor

--- skiff/__init__.py
+++ skiff/__init__.py
@@ -8,5 +8,5 @@
 # http://www.opensource.org/licenses/MIT-license
 # Copyright (c) 2014 Matt Condon m@cond.in
 
-from . import (Action, Domain, Droplet, Image, Kernel, Key, Region, Size)
-from .utils import token
+# from . import (Action, Domain, Droplet, Image, Kernel, Key, Region, Size)
+from .utils import SkiffClass
--- skiff/utils.py
+++ skiff/utils.py
@@ -1,19 +1,25 @@
 
-DO_TOKEN = None
-
 DO_BASE_URL = "https://api.digitalocean.com/v2"
-DO_HEADERS = {
-    "Content-Type": "application/json"
-}
-DO_DELETE_HEADERS = {
-    "Content-Type": "application/x-www-form-urlencoded"
-}
-
-# @TODO: write decorator that makes sure that token is set
 
 
-def token(token):
-    DO_TOKEN = token
-    DO_HEADERS['Authorization'] = "Bearer " + DO_TOKEN
-    DO_DELETE_HEADERS['Authorization'] = "Bearer " + DO_TOKEN
-    return True
+class SkiffClass(object):
+    """docstring for SkiffClass"""
+
+    from . import (Action, Domain, Droplet, Image, Kernel, Key, Region, Size)
+
+    DO_HEADERS = {
+        "Content-Type": "application/json"
+    }
+    DO_DELETE_HEADERS = {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+
+    def __init__(self, token):
+        super(SkiffClass, self).__init__()
+        self.DO_TOKEN = DO_TOKEN
+        self.DO_HEADERS['Authorization'] = "Bearer " + self.DO_TOKEN
+        self.DO_DELETE_HEADERS['Authorization'] = "Bearer " + self.DO_TOKEN
+
+
+def rig(token):
+    return SkiffClass(token)
