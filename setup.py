03f7f5c91f613f7a4a3fbfbfb24d2391b0cc7fdb
Matt Condon <MattCMultimedia@gmail.com>
2014-08-16 14:24:14 -0500

    add _json to SkiffDomainRecord

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ tests_require = [
 
 setup(
     name='skiff',
-    version='0.9.1',
+    version='0.9.2',
     description="Python library for DigitalOcean's v2 API",
     long_description='''
 Python library for DigitalOcean's v2 API
